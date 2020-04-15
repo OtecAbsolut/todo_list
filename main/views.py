c5eb8a546681fa0fe17307fefffe13b85eb42956
serg <3481126@mail.ru>
2020-04-15 21:16:13 +0300

    Revert "Результат второго урока"
    This reverts commit fc7fa854

--- main/views.py
+++ main/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
 
 
-data = {
-    'lists': [
-        {'name': 'Работа', 'is_done': True, 'date': '01.12.2019'},
-        {'name': 'Дом', 'is_done': False},
-        {'name': 'Учеба', 'is_done': True}
-    ],
-    'user_name': 'Admin',
-}
-
-
 def main_view(request):
-    context = data
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
